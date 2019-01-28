038d087d7799706478ff84e4fd37ba9f0dd93804
Yiheng Li <liyiheng.scott@gmail.com>
2019-01-27 23:25:21 -0800

    🚧 [report] added echarts rolling returns

--- valpy/backtest/report.py
+++ valpy/backtest/report.py
@@ -1,4 +1,6 @@
 from . import pyfolio
+from . import plotting
+from IPython.core.display import display
 
 
 class ReportBuilder(object):
@@ -12,6 +14,10 @@ class ReportBuilder(object):
   <meta charset="utf-8">
   <meta name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Echarts JS -->
+  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/echarts/4.1.0/echarts-en.min.js"></script>
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet"
@@ -27,10 +33,20 @@ class ReportBuilder(object):
     <h1>{report_name}</h1>
     <h2>Performance Table</h2>
     {table}
+    <h2>Returns Analysis</h2>
+      <div id="rolling_cum_returns" style="width: 100%;height:500px;"></div>
+      <script type="text/javascript">
+        var chartRCT = echarts.init(document.getElementById('rolling_cum_returns'));
+        {rct_func}
+        var optionRCT = {rct_option};
+        chartRCT.setOption(optionRCT);
+        $(window).on('resize', function(){{
+          if(chartRCT != null && chartRCT != undefined){{chartRCT.resize();}} }});
+    </script>
   </div>
 </body>
 </html>
-"""
+""" # noqa E501
 
     def __init__(self, strat, benchmark_rets, live_start_date,
                  report_name='Report'):
@@ -51,11 +67,19 @@ class ReportBuilder(object):
 
         table = self.get_performance_table(jupyter=jupyter)
 
+        if jupyter:
+            display(self.get_interactive_rolling_returns(jupyter=jupyter))
+
         if dest is not None:
+            rct_f, rct_o = self.get_interactive_rolling_returns(
+                jupyter=jupyter)
+
             with open(dest, 'w') as report:
                 report.write(self.template.format(
                     report_name=self.report_name,
-                    table=table))
+                    table=table,
+                    rct_func=rct_f,
+                    rct_option=rct_o))
 
     def get_performance_table(self, jupyter=True):
         return pyfolio.show_perf_stats(returns=self.returns,
@@ -63,3 +87,28 @@ class ReportBuilder(object):
                                        transactions=self.transactions,
                                        live_start_date=self.live_start_date,
                                        jupyter=jupyter)
+
+    def get_interactive_rolling_returns(self, jupyter=True):
+        plot = plotting.plot_interactive_rolling_returns(
+            returns=self.returns,
+            factor_returns=self.benchmark_rets,
+            live_start_date=self.live_start_date,
+            cone_std=[1, 1.5, 2]
+        )
+
+        if jupyter:
+            return plot
+        else:
+            html_text = plot._repr_html_()
+            chart_id = plot.chart_id
+
+            func_start = html_text.find('function tooltip_format(params)')
+            func_end = html_text.find('var option_{}'.format(chart_id))
+            option_start = html_text.find(
+                '{} = {{\n    "title": '.format(chart_id))
+            option_end = html_text.find('\nmyChart_{}'.format(chart_id))
+
+            func_str = html_text[func_start:func_end]
+            option_str = html_text[option_start + len(chart_id) + 3:option_end]
+
+            return func_str, option_str
