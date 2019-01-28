d6077dc3ed14283abd4264f2cceb46c3ebf5c512
Yiheng Li <liyiheng.scott@gmail.com>
2019-01-27 23:25:00 -0800

    ✨ [bt] added eacharts rolling returns

---
+++ valpy/backtest/plotting.py
@@ -0,0 +1,165 @@
+from pyecharts import Line
+from .pyfolio import get_rolling_returns
+import numpy as np
+
+
+class Number:
+    pass
+
+
+class PlottingColors:
+
+    CI_AREA_COLOR = '#0082c8'
+    GREEN = '#3cb44b'
+    PINK_RED = '#e6194b'
+
+
+def plot_interactive_rolling_returns(returns,
+                                     factor_returns=None,
+                                     live_start_date=None,
+                                     cone_std=None):
+    """
+    Plots cumulative rolling returns versus some benchmarks'.
+
+    Backtest returns are in green, and out-of-sample (live trading)
+    returns are in red.
+
+    Additionally, a non-parametric cone plot may be added to the
+    out-of-sample returns region.
+
+    Parameters
+    ----------
+    returns : pd.Series
+        Daily returns of the strategy, noncumulative.
+         - See full explanation in tears.create_full_tear_sheet.
+    factor_returns : pd.Series, optional
+        Daily noncumulative returns of the benchmark factor to which betas are
+        computed. Usually a benchmark such as market returns.
+         - This is in the same style as returns.
+    live_start_date : datetime, optional
+        The date when the strategy began live trading, after
+        its backtest period. This date should be normalized.
+    logy : bool, optional
+        Whether to log-scale the y-axis.
+    cone_std : float, or tuple, optional
+        If float, The standard deviation to use for the cone plots.
+        If tuple, Tuple of standard deviation values to use for the cone plots
+         - See timeseries.forecast_cone_bounds for more details.
+    legend_loc : matplotlib.loc, optional
+        The location of the legend on the plot.
+    volatility_match : bool, optional
+        Whether to normalize the volatility of the returns to those of the
+        benchmark returns. This helps compare strategies with different
+        volatilities. Requires passing of benchmark_rets.
+    cone_function : function, optional
+        Function to use when generating forecast probability cone.
+        The function signiture must follow the form:
+        def cone(in_sample_returns (pd.Series),
+                 days_to_project_forward (int),
+                 cone_std= (float, or tuple),
+                 starting_value= (int, or float))
+        See timeseries.forecast_cone_bootstrap for an example.
+    ax : matplotlib.Axes, optional
+        Axes upon which to plot.
+    **kwargs, optional
+        Passed to plotting function.
+
+    Returns
+    -------
+    ax : matplotlib.Axes
+        The axes that were plotted on.
+    """
+
+    def tooltip_format(params):
+        def get_color(color):
+            return '<span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:' + color + '"></span>' # noqa E501
+
+        res = params[0].axisValue
+        for i in params:
+            if i.seriesName != 'CI' and not Number.isNaN(i.data[1]):
+                value = (i.data[1] - 1) * 100
+                res = res + '<br>' + \
+                    get_color(i.color) + ' ' + i.seriesName + \
+                    ': ' + value.toFixed(2) + '%'
+
+        return res
+
+    metrics = get_rolling_returns(returns, factor_returns=factor_returns,
+                                  live_start_date=live_start_date,
+                                  cone_std=cone_std)
+
+    mini_list = [np.min(metrics['cum_factor_returns']),
+                 np.min(metrics['is_cum_returns'])]
+
+    if metrics['oos_cum_returns'] is not None:
+        mini_list.append(np.min(metrics['oos_cum_returns']))
+    if metrics['cone_bounds'] is not None:
+        mini_list.append(min(np.min(metrics['cone_bounds'])))
+    yaxis_min = np.round(min(mini_list) - 0.05, 2)
+
+    u_list, d_list = [], []
+    if metrics['cone_bounds'] is not None:
+        for cone_value in metrics['cone_bounds'].columns.tolist():
+            if cone_value > 0:
+                u_list.append(metrics['cone_bounds'][cone_value])
+            else:
+                d_list.append(metrics['cone_bounds'][cone_value])
+
+    attr = metrics['cum_factor_returns'].index.strftime('%Y-%m-%d').tolist()
+    line = Line("Cumulative Returns")
+
+    # Set Up Color Variables
+    area_color = PlottingColors.CI_AREA_COLOR
+
+    # Add Benchmark Line
+    line.add("Benchmark", attr,
+             np.round(metrics['cum_factor_returns'], 4).tolist(),
+             tooltip_trigger='axis', is_datazoom_show=True, line_width=2,
+             line_opacity=0.8, datazoom_range=[0, 100])
+    line._option['color'][0] = 'grey'
+
+    # Add Confidence Interval
+    ci_index = 1
+    for u, d in zip(u_list, d_list):
+        line.add("CI", attr, np.round(d, 4).tolist(), line_opacity=0,
+                 is_stack=True, is_label_emphasis=False, is_symbol_show=False)
+        line.add("CI", attr, np.round(u-d, 4).tolist(), line_opacity=0,
+                 is_label_emphasis=False, area_color=area_color,
+                 area_opacity=0.15, is_stack=True, is_symbol_show=False)
+        line._option['series'][ci_index]['stack'] = 'CI{}'.format(ci_index)
+        line._option['series'][ci_index+1]['stack'] = 'CI{}'.format(ci_index)
+        ci_index += 2
+
+    # Add OOS Line
+    if metrics['oos_cum_returns'] is not None:
+        line.add("Out Of Sample", attr,
+                 np.round(metrics['oos_cum_returns'], 4).tolist(),
+                 tooltip_trigger='axis', line_width=3, line_opacity=0.8,
+                 is_symbol_show=False)
+        line._option['color'][2] = PlottingColors.PINK_RED
+        line._option['color'][3] = PlottingColors.GREEN
+    else:
+        line._option['color'][1] = PlottingColors.GREEN
+
+    # Add IS Line
+    line.add("In Sample", attr,
+             np.round(metrics['is_cum_returns'], 4).tolist(),
+             tooltip_trigger='axis', tooltip_formatter=tooltip_format,
+             yaxis_min=yaxis_min, line_width=3, line_opacity=0.8,
+             is_symbol_show=False)
+
+    line._option['legend'][0]['data'] = [
+        'Benchmark', 'In Sample', 'Out Of Sample', 'CI']
+
+    # Set up colors
+    if metrics['oos_cum_returns'] is None:
+        line._option['color'][1] = PlottingColors.GREEN
+    elif metrics['oos_cum_returns'] is not None and \
+            metrics['cone_bounds'] is None:
+        line._option['color'][1] = PlottingColors.PINK_RED
+        line._option['color'][2] = PlottingColors.GREEN
+    else:
+        line._option['color'][2] = PlottingColors.PINK_RED
+        line._option['color'][3] = PlottingColors.GREEN
+
+    return line
--- valpy/backtest/pyfolio.py
+++ valpy/backtest/pyfolio.py
@@ -305,3 +305,117 @@ def show_perf_stats(returns, factor_returns=None, positions=None,
             '<th>In-sample</th>\n      <th>Out-of-sample</th>\n'
         )
         return html
+
+
+def get_rolling_returns(returns,
+                        factor_returns=None,
+                        live_start_date=None,
+                        logy=False,
+                        cone_std=None,
+                        legend_loc='best',
+                        volatility_match=False,
+                        cone_function=timeseries.forecast_cone_bootstrap):
+    """
+    Plots cumulative rolling returns versus some benchmarks'.
+
+    Backtest returns are in green, and out-of-sample (live trading)
+    returns are in red.
+
+    Additionally, a non-parametric cone plot may be added to the
+    out-of-sample returns region.
+
+    Parameters
+    ----------
+    returns : pd.Series
+        Daily returns of the strategy, noncumulative.
+         - See full explanation in tears.create_full_tear_sheet.
+    factor_returns : pd.Series, optional
+        Daily noncumulative returns of the benchmark factor to which betas are
+        computed. Usually a benchmark such as market returns.
+         - This is in the same style as returns.
+    live_start_date : datetime, optional
+        The date when the strategy began live trading, after
+        its backtest period. This date should be normalized.
+    logy : bool, optional
+        Whether to log-scale the y-axis.
+    cone_std : float, or tuple, optional
+        If float, The standard deviation to use for the cone plots.
+        If tuple, Tuple of standard deviation values to use for the cone plots
+         - See timeseries.forecast_cone_bounds for more details.
+    legend_loc : matplotlib.loc, optional
+        The location of the legend on the plot.
+    volatility_match : bool, optional
+        Whether to normalize the volatility of the returns to those of the
+        benchmark returns. This helps compare strategies with different
+        volatilities. Requires passing of benchmark_rets.
+    cone_function : function, optional
+        Function to use when generating forecast probability cone.
+        The function signiture must follow the form:
+        def cone(in_sample_returns (pd.Series),
+                 days_to_project_forward (int),
+                 cone_std= (float, or tuple),
+                 starting_value= (int, or float))
+        See timeseries.forecast_cone_bootstrap for an example.
+
+    Returns
+    -------
+    ax : matplotlib.Axes
+        The axes that were plotted on.
+    """
+
+    if volatility_match and factor_returns is None:
+        raise ValueError('volatility_match requires passing of '
+                         'factor_returns.')
+    elif volatility_match and factor_returns is not None:
+        bmark_vol = factor_returns.loc[returns.index].std()
+        returns = (returns / returns.std()) * bmark_vol
+
+    cum_rets = ep.cum_returns(returns, 1.0)
+
+    if factor_returns is not None:
+        cum_factor_returns = ep.cum_returns(
+            factor_returns[cum_rets.index], 1.0)
+
+    if live_start_date is not None:
+        live_start_date = ep.utils.get_utc_timestamp(live_start_date)
+        is_cum_returns = cum_rets.loc[cum_rets.index < live_start_date]
+        oos_cum_returns = cum_rets.loc[cum_rets.index >= live_start_date]
+    else:
+        is_cum_returns = cum_rets
+        oos_cum_returns = pd.Series([])
+
+    if len(oos_cum_returns) > 0:
+
+        if cone_std is not None:
+            if isinstance(cone_std, (float, int)):
+                cone_std = [cone_std]
+
+            is_returns = returns.loc[returns.index < live_start_date]
+            cone_bounds = cone_function(
+                is_returns,
+                len(oos_cum_returns),
+                cone_std=cone_std,
+                starting_value=is_cum_returns[-1])
+            cone_bounds.index = oos_cum_returns.index
+        else:
+            cone_bounds = None
+    else:
+        cone_bounds = None
+
+    is_cum_returns = pd.Series(is_cum_returns, index=cum_factor_returns.index)
+
+    if len(oos_cum_returns) > 0:
+        oos_cum_returns = pd.Series(oos_cum_returns,
+                                    index=cum_factor_returns.index)
+    else:
+        oos_cum_returns = None
+
+    if cone_bounds is not None:
+        cone_bounds = pd.DataFrame(cone_bounds, index=cum_factor_returns.index)
+
+    return {
+        'cum_factor_returns': cum_factor_returns,
+        'is_cum_returns': is_cum_returns,
+        'oos_cum_returns': oos_cum_returns,
+        'cone_bounds': cone_bounds
+    }
